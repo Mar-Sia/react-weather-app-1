afbb8b0758ea54de14165f70a64cd9b671761520
Mar-Sia <98830671+Mar-Sia@users.noreply.github.com>
2022-09-03 13:46:58 +0800

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -3,16 +3,18 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      Weather App
-      <footer>
-        This project was coded by Maria Siara and is
-        <a
-          href="https://github.com/Mar-Sia/react-weather-app-1"
-          target="_blank"
-        >
-          open-sourced on GitHub
-        </a>
-      </footer>
+      <div className="container">
+        Weather App
+        <footer>
+          This project was coded by Maria Siara and is
+          <a
+            href="https://github.com/Mar-Sia/react-weather-app-1"
+            target="_blank"
+          >
+            open-sourced on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
